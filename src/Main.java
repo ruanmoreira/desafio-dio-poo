fad85dd8028e8090b66dbfebdb20420aedcfadb1
Ruan Moreira <ruaanmoreira2508@gmail.com>
2023-01-18 13:03:07 -0300

    Aplicando a teoria de encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso("Curso: Java", "Descrição curso java", 8);
+        Curso curso2 = new Curso("Curso: js", "Descrição curso js", 4);
+
+        Mentoria ment1 = new Mentoria("Mentoria de java", "descrição mentoria java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(ment1);
     }
 }
